f3506a310ba86c7b319041ce4ea148b115d1dddd
jarosquijano-netizen <jarosquijano-netizen@users.noreply.github.com>
2026-01-08 13:35:06 +0100

    fix: Remove extra closing div tag in AI Message Generator section

--- src/components/CardDetailModal.tsx
+++ src/components/CardDetailModal.tsx
@@ -899,7 +899,6 @@ export function CardDetailModal({
                     </div>
                   </div>
                 </div>
-              </div>
             </div>
 
             {/* Activity Section - Moved to end */}
